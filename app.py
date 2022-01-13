e55b7d243a153bb46c008c2667f5c52eb5f5e4ec
Nahid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-01-13 14:20:00 +0600

    added some changes on github

---
+++ app.py
@@ -0,0 +1,16 @@
+# a = str(input("Enter your text:"))
+# b = "10" + "100" + "1000"
+# c = a + b
+# d = c.split(",")
+# print(c)
+# print(d)
+
+a = input("TEXT : ")
+b = ",11,22,33"
+aa = a.split(",")
+bb = b.split(",")
+ab = a+b
+
+# print(aa)
+print(bb)
+# print(ab)
